edf5e94367de6f09b4341fd03e0ffa8f4e181937
mmhlego <mmhlego@gmail.com>
2023-06-25 21:22:57 +0330

    Merge conflict fixed

--- src/Products.API/Controllers/CategoryController.cs
+++ src/Products.API/Controllers/CategoryController.cs
@@ -25,13 +25,6 @@ namespace Products.API.Controllers
 			var categories = _categoryService.GetCategories();
 			return Ok(categories);
 		}
-		[HttpGet]
-		[Route("categories")]
-		public ActionResult<List<Category>> GetCategories()
-		{
-			var categories = _categoryService.GetCategories();
-			return Ok(categories);
-		}
 
 		[HttpGet]
 		[Route("subcategories")]
@@ -49,9 +42,8 @@ namespace Products.API.Controllers
 			return Ok(subcategories);
 		}
 
-
 		[HttpPost]
-		[Route("/categories")]
+		[Route("categories")]
 		[Authorize(Roles = "Admin , StoreKeeper")]
 		public ActionResult<StatusResponse> AddCategory([FromBody] CategoryRequests categoryRequest)
 		{
@@ -59,124 +51,73 @@ namespace Products.API.Controllers
 			{
 				return BadRequest(StatusResponse.Failed("اطلاعات دسته بندی را وارد نکردید."));
 			}
-			[HttpPost]
-			[Route("categories")]
-			[Authorize(Roles = "Admin , StoreKeeper")]
-			public ActionResult<StatusResponse> AddCategory([FromBody] CategoryRequests categoryRequest)
-			{
-				if (categoryRequest == null)
-				{
-					return BadRequest(StatusResponse.Failed("اطلاعات دسته بندی را وارد نکردید."));
-				}
 
-				_categoryService.AddCategory(categoryRequest.Title, categoryRequest.IconName);
-				return Ok(StatusResponse.Success);
+			_categoryService.AddCategory(categoryRequest.Title, categoryRequest.IconName);
+			return Ok(StatusResponse.Success);
+		}
+
+		[HttpPost]
+		[Route("subcategories")]
+		[Authorize(Roles = "Admin , StoreKeeper , Seller")]
+		public ActionResult AddSubcategory([FromBody] SubcategoryRequests subcategoryRequests)
+		{
+			if (_categoryService.GetCategoryById(subcategoryRequests.CategoryId) == null)
+				return NotFound(StatusResponse.Failed("دسته بندی مورد نظر پیدا نشد."));
+			_categoryService.AddSubcategory(subcategoryRequests.CategoryId, subcategoryRequests.Title);
+			return Ok(StatusResponse.Success);
+		}
+
+		[HttpPut]
+		[Route("categories/{id}")]
+		[Authorize(Roles = "Admin , StoreKeeper")]
+		public ActionResult UpdateCategory(Guid id, [FromBody] CategoryRequests categoryRequests)
+		{
+			var category = _categoryService.GetCategoryById(id);
+			if (category == null)
+				return NotFound(StatusResponse.Failed("دسته بندی مورد نظر پیدا نشد."));
+			category.Title = categoryRequests.Title;
+			category.IconName = categoryRequests.IconName;
+			_categoryService.UpdateCategory(category);
+			return Ok(StatusResponse.Success);
+		}
+
+		[HttpPut]
+		[Route("subcategories/{id}/fields")]
+		[Authorize(Roles = "Admin , StoreKeeper , Seller")]
+		public ActionResult<StatusResponse> UpdateSubcategoryFields(Guid id, [FromBody] List<Field> fields)
+		{
+			if (id == default)
+				return BadRequest(StatusResponse.Failed(" اطلاعات زیردسته را وارد نکردید."));
+
+			if (_categoryService.GetSubcategoryById(id) == null)
+				return NotFound(StatusResponse.Failed("زیردسته مورد نظر پیدا نشد."));
+
+			foreach (Field f in fields)
+			{
+				if (f.SubcategoryId != id)
+					return BadRequest(StatusResponse.Failed("خطایی رخ داده."));
+				if (_categoryService.GetSubcategoryById(id) == null)
+					return NotFound(StatusResponse.Failed("زیردسته مورد نظر پیدا نشد."));
+
+				_categoryService.UpdateField(f.Id, f.Title);
 			}
 
-			[HttpPost]
-			[Route("subcategories")]
-			[Authorize(Roles = "Admin , StoreKeeper , Seller")]
-			public ActionResult AddSubcategory([FromBody] SubcategoryRequests subcategoryRequests)
-			{
-				if (_categoryService.GetCategoryById(subcategoryRequests.CategoryId) == null)
-					return NotFound(StatusResponse.Failed("دسته بندی مورد نظر پیدا نشد."));
-				_categoryService.AddSubcategory(subcategoryRequests.CategoryId, subcategoryRequests.Title);
-				return Ok(StatusResponse.Success);
-			}
+			return Ok(StatusResponse.Success);
+		}
 
-			[HttpPut]
-			[Route("categories/{id}")]
-			[Authorize(Roles = "Admin , StoreKeeper")]
-			public ActionResult UpdateCategory(Guid id, [FromBody] CategoryRequests categoryRequests)
-			{
-				var category = _categoryService.GetCategoryById(id);
-				if (category == null)
-					return NotFound(StatusResponse.Failed("دسته بندی مورد نظر پیدا نشد."));
-				category.Title = categoryRequests.Title;
-				category.IconName = categoryRequests.IconName;
-				_categoryService.UpdateCategory(category);
-				return Ok(StatusResponse.Success);
-			}
+		[HttpPost]
+		[Route("subcategories/{id}/fields")]
+		[Authorize(Roles = "Admin , StoreKeeper , Seller")]
+		public ActionResult<StatusResponse> AddSubcategoryFields(Guid id, string title)
+		{
+			if (id == default)
+				return BadRequest(StatusResponse.Failed(" اطلاعات دسته بندی را وارد نکردید."));
 
-			[HttpPut]
-			[Route("/subcategories/{id}")]
-			[Authorize(Roles = "Admin , StoreKeeper , Seller")]
-			public ActionResult<StatusResponse> UpdateSubcategory(Guid id, [FromBody] SubcategoryRequests subcategoryRequests)
-			{
-				var subcategory = _categoryService.GetSubcategoryById(id);
-				[HttpPut]
-				[Route("subcategories/{id}")]
-				[Authorize(Roles = "Admin , StoreKeeper , Seller")]
-				public ActionResult<StatusResponse> UpdateSubcategory(Guid id, [FromBody] SubcategoryRequests subcategoryRequests)
-				{
-					var subcategory = _categoryService.GetSubcategoryById(id);
+			if (_categoryService.GetSubcategoryById(id) == null)
+				return NotFound(StatusResponse.Failed("دسته بندی مورد نظر پیدا نشد."));
 
-					if (subcategory == null)
-						return NotFound(StatusResponse.Failed("زیردسته مورد نظر پیدا نشد."));
-					subcategory.Title = subcategoryRequests.Title;
-					subcategory.CategoryId = subcategoryRequests.CategoryId;
-					_categoryService.UpdateSubcategory(subcategory);
-					return Ok(StatusResponse.Success);
-				}
-
-				[HttpPut]
-				[Route("/subcategories/{id}/fields")]
-				[Authorize(Roles = "Admin , StoreKeeper , Seller")]
-				public ActionResult<StatusResponse> UpdateSubcategoryFields(Guid id, [FromBody] List<Field> fields)
-				{
-					if (id == default(Guid))
-						return BadRequest(StatusResponse.Failed(" اطلاعات زیردسته را وارد نکردید."));
-					[HttpPut]
-					[Route("subcategories/{id}/fields")]
-					[Authorize(Roles = "Admin , StoreKeeper , Seller")]
-					public ActionResult<StatusResponse> UpdateSubcategoryFields(Guid id, [FromBody] List<Field> fields)
-					{
-						if (id == default(Guid))
-							return BadRequest(StatusResponse.Failed(" اطلاعات زیردسته را وارد نکردید."));
-
-						if (_categoryService.GetSubcategoryById(id) == null)
-							return NotFound(StatusResponse.Failed("زیردسته مورد نظر پیدا نشد."));
-
-						foreach (Field f in fields)
-						{
-							if (f.SubcategoryId != id)
-								return BadRequest(StatusResponse.Failed("خطایی رخ داده."));
-							if (_categoryService.GetSubcategoryById(id) == null)
-								return NotFound(StatusResponse.Failed("زیردسته مورد نظر پیدا نشد."));
-
-							foreach (Field f in fields)
-							{
-								if (_categoryService.GetFieldById(f.Id) == null)
-									return BadRequest(StatusResponse.Failed("فیلد مورد نظر وجود ندارد."));
-								if (f.SubcategoryId != id)
-									return BadRequest(StatusResponse.Failed("خطایی رخ داده."));
-
-								_categoryService.UpdateField(f.Id, f.Title);
-							}
-
-							return Ok(StatusResponse.Success);
-						}
-
-						[HttpPost]
-						[Route("/subcategories/{id}/fields")]
-						[Authorize(Roles = "Admin , StoreKeeper , Seller")]
-						public ActionResult<StatusResponse> AddSubcategoryFields(Guid id, string title)
-						{
-							if (id == default(Guid))
-								return BadRequest(StatusResponse.Failed(" اطلاعات دسته بندی را وارد نکردید."));
-							[HttpPost]
-							[Route("subcategories/{id}/fields")]
-							[Authorize(Roles = "Admin , StoreKeeper , Seller")]
-							public ActionResult<StatusResponse> AddSubcategoryFields(Guid id, string title)
-							{
-								if (id == default(Guid))
-									return BadRequest(StatusResponse.Failed(" اطلاعات دسته بندی را وارد نکردید."));
-
-								if (_categoryService.GetSubcategoryById(id) == null)
-									return NotFound(StatusResponse.Failed("دسته بندی مورد نظر پیدا نشد."));
-
-								_categoryService.AddField(id, title);
-								return Ok(StatusResponse.Success);
-							}
-						}
-					}
+			_categoryService.AddField(id, title);
+			return Ok(StatusResponse.Success);
+		}
+	}
+}
