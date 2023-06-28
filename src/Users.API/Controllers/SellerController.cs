a8e93e2c85eae211c51988cbf81ec28fd3172ee0
mmhlego <mmhlego@gmail.com>
2023-06-29 00:50:04 +0330

    Bug fixes

--- src/Users.API/Controllers/SellerController.cs
+++ src/Users.API/Controllers/SellerController.cs
@@ -9,142 +9,141 @@ using Users.API.Services;
 
 namespace Users.API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]/")]
-    public class SellerController : ControllerBase
-    {
+	[ApiController]
+	[Route("api/[controller]/")]
+	public class SellerController : ControllerBase
+	{
 
-        private readonly IUsersService _seller;
-        public SellerController(UsersService seller)
-        {
-            _seller = seller;
-        }
+		private readonly IUsersService _seller;
+		public SellerController(IUsersService seller)
+		{
+			_seller = seller;
+		}
 
-        [HttpGet]
-        [Route("sellers")]
-        public ActionResult<List<SellerInfo>> GetSellers(int page = 0, int perPage = 0, string searchText = "")
-        {
-            List<User> users = _seller.GetUsers().Where(u => u.Type == UserTypes.Seller).ToList();
-            List<Seller> sellers = _seller.GetSellers();
-            List<Seller> sellerInfoList = _seller.GetSellers();
+		[HttpGet]
+		[Route("sellers")]
+		public ActionResult<Pagination<SellerInfo>> GetSellers(int page = 0, int perPage = 0, string searchText = "")
+		{
+			List<User> users = _seller.GetUsers().Where(u => u.Type == UserTypes.Seller).ToList();
+			List<Seller> sellers = _seller.GetSellers();
+			List<Seller> sellerInfoList = _seller.GetSellers();
 
-            List<SellerInfo> sellersForShow = users
-                .Join(sellerInfoList, user => user.Id, seller => seller.UserId, (user, seller) => sellerInfo(seller,user))
-                .ToList();
+			List<SellerInfo> sellersForShow = users
+				.Join(sellerInfoList, user => user.Id, seller => seller.UserId, (user, seller) => sellerInfo(seller, user))
+				.ToList();
 
-            if (searchText != "")
-            {
-                sellersForShow = sellersForShow
-            .Where(seller => seller.Username.Contains(searchText)
-                             || seller.Address.Contains(searchText)
-                             || seller.Information.Contains(searchText))
-            .ToList();
+			if (searchText != "")
+			{
+				sellersForShow = sellersForShow
+			.Where(seller => seller.Username.Contains(searchText)
+							 || seller.Address.Contains(searchText)
+							 || seller.Information.Contains(searchText))
+			.ToList();
 
-            }
-            Pagination<SellerInfo> sellerPagination = Pagination<SellerInfo>.Paginate(sellersForShow, perPage, page);
-            return Ok(sellerPagination);
-        }
+			}
+			Pagination<SellerInfo> sellerPagination = Pagination<SellerInfo>.Paginate(sellersForShow, perPage, page);
+			return Ok(sellerPagination);
+		}
 
-        [HttpGet]
-        [Route("sellers/{id}")]
-        public ActionResult<Seller> GetSeller(Guid userId)
-        {
-            Seller? seller = _seller.GetSellerByUserId(userId);
-            if (seller == null)
-            {
-                return Ok(StatusResponse.Failed("فروشنده موردنظر یافت نشد"));
-            }
+		[HttpGet]
+		[Route("sellers/{id}")]
+		public ActionResult<SellerInfo> GetSeller(Guid userId)
+		{
+			Seller? seller = _seller.GetSellerByUserId(userId);
+			if (seller == null)
+			{
+				return Ok(StatusResponse.Failed("فروشنده موردنظر یافت نشد"));
+			}
 
-            return Ok(seller);
-        }
+			return Ok(seller);
+		}
 
-        [HttpPut]
-        [Route("sellers/{id}")]
-        [Authorize(Roles = "Admin,Owner")]
-        public ActionResult<Seller> UpdateSeller(Guid userId, [FromBody] UpdateSeller request)
-        {
-            Seller? seller = _seller.GetSellerByUserId(userId);
-            if (seller == null)
-            {
-                return Ok(StatusResponse.Failed("فروشنده  موردنظر یافت نشد"));
-            }
+		[HttpPut]
+		[Route("sellers/{id}")]
+		[Authorize(Roles = "Admin,Owner")]
+		public ActionResult<SellerInfo> UpdateSeller(Guid userId, [FromBody] UpdateSeller request)
+		{
+			Seller? seller = _seller.GetSellerByUserId(userId);
+			if (seller == null)
+			{
+				return Ok(StatusResponse.Failed("فروشنده  موردنظر یافت نشد"));
+			}
 
-            seller.Information = request.Information;
-            seller.Address = request.Address;
-            _seller.UpdateSeller(seller);
-            return Ok(seller);
-        }
+			seller.Information = request.Information;
+			seller.Address = request.Address;
+			_seller.UpdateSeller(seller);
+			return Ok(seller);
+		}
 
-        [HttpDelete]
-        [Route("sellers/{id}")]
-        [Authorize(Roles = "Admin,Owner")]
-        public ActionResult<Seller> DeleteSeller(Guid userId)
-        {
-            User? seller = _seller.GetUserById(userId);
-            if (seller == null)
-            {
-                return Ok(StatusResponse.Failed("فروشنده  موردنظر یافت نشد"));
-            }
-            else
-            {
-                seller.Restricted = true;
-                _seller.UpdateUser(seller);
-                return Ok(seller);
-            }
-        }
+		[HttpDelete]
+		[Route("sellers/{id}")]
+		[Authorize(Roles = "Admin,Owner")]
+		public ActionResult<SellerInfo> DeleteSeller(Guid userId)
+		{
+			User? seller = _seller.GetUserById(userId);
+			if (seller == null)
+			{
+				return Ok(StatusResponse.Failed("فروشنده  موردنظر یافت نشد"));
+			}
 
-        [HttpGet]
-        [Route("profile")]
-        [Authorize(Roles = "Seller")]
-        public ActionResult<SellerInfo> GetProfile()
-        {
-            Guid.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out Guid currentUser);
-            User user = _seller.GetUserById(currentUser)!;
-            Seller seller = _seller.GetSellerByUserId(currentUser)!;
-            SellerInfo sellerForShow = sellerInfo(seller, user);
-            return Ok(sellerForShow);
-        }
+			seller.Restricted = true;
+			_seller.UpdateUser(seller);
+			return Ok(seller);
+		}
 
-        [HttpPut]
-        [Route("profile")]
-        [Authorize(Roles = "Seller")]
-        public ActionResult<SellerInfo> UpdateProfile([FromBody] SellerInfo profile)
-        {
-            Guid.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out Guid currentUser);
-            Seller seller = _seller.GetSellerByUserId(currentUser)!;
-            User user = _seller.GetUserById(currentUser)!;
+		[HttpGet]
+		[Route("profile")]
+		[Authorize(Roles = "Seller")]
+		public ActionResult<SellerInfo> GetProfile()
+		{
+			Guid.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out Guid currentUser);
+			User user = _seller.GetUserById(currentUser)!;
+			Seller seller = _seller.GetSellerByUserId(currentUser)!;
+			SellerInfo sellerForShow = sellerInfo(seller, user);
+			return Ok(sellerForShow);
+		}
 
-            user.PhoneNumber = profile.PhoneNumber;
-            user.LastName = profile.LastName;
-            user.FirstName = profile.FirstName;
-            user.Email = profile.Email;
-            user.BirthDate = profile.BirthDate;
-            user.Avatar = profile.Avatar;
+		[HttpPut]
+		[Route("profile")]
+		[Authorize(Roles = "Seller")]
+		public ActionResult<SellerInfo> UpdateProfile([FromBody] SellerInfo profile)
+		{
+			_ = Guid.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out Guid currentUser);
+			Seller seller = _seller.GetSellerByUserId(currentUser)!;
+			User user = _seller.GetUserById(currentUser)!;
 
-            _seller.UpdateUser(user);
+			user.PhoneNumber = profile.PhoneNumber;
+			user.LastName = profile.LastName;
+			user.FirstName = profile.FirstName;
+			user.Email = profile.Email;
+			user.BirthDate = profile.BirthDate;
+			user.Avatar = profile.Avatar;
 
-            seller.Address = profile.Address;
-            seller.Information = profile.Information;
+			_seller.UpdateUser(user);
 
-            _seller.UpdateSeller(seller);
-            return Ok(profile);
-        }
-        private SellerInfo sellerInfo(Seller seller, User user)
-        {
-            SellerInfo output = new SellerInfo
-            {
-                Username = user.Username,
-                Password = user.Password,
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                BirthDate = user.BirthDate,
-                Avatar = user.Avatar,
-                Email = user.Email,
-                PhoneNumber = user.PhoneNumber,
-                Address = seller.Address,
-                Information = seller.Information
-            };
-            return output;
-        }
-    }
+			seller.Address = profile.Address;
+			seller.Information = profile.Information;
+
+			_seller.UpdateSeller(seller);
+			return Ok(profile);
+		}
+
+		private SellerInfo sellerInfo(Seller seller, User user)
+		{
+			SellerInfo output = new SellerInfo
+			{
+				Username = user.Username,
+				Password = user.Password,
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				BirthDate = user.BirthDate,
+				Avatar = user.Avatar,
+				Email = user.Email,
+				PhoneNumber = user.PhoneNumber,
+				Address = seller.Address,
+				Information = seller.Information
+			};
+			return output;
+		}
+	}
 }
