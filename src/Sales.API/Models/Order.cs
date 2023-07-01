fe5b296d7677647fcdb0fdad1a1eda512fcd66df
mmhlego <mmhlego@gmail.com>
2023-07-01 09:14:47 +0330

    Bug Fixes

--- src/Sales.API/Models/Order.cs
+++ src/Sales.API/Models/Order.cs
@@ -2,24 +2,34 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using SharedModels;
 
-namespace Sales.API.Models {
-    public class Order {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public OrderStates State { get; set; } = OrderStates.Filling;
+namespace Sales.API.Models
+{
+	public class Order
+	{
+		public Guid Id { get; set; } = Guid.NewGuid();
+		public OrderStates State { get; set; } = OrderStates.Filling;
 
-        public long TotalPrice {
-            get {
-                long price = 0;
+		public long TotalPrice
+		{
+			get
+			{
+				long price = 0;
 
-                foreach (var item in OrderItems) {
-                    price += item.Amount * item.SalePrice.Price;
-                }
+				if (OrderItems == null)
+				{
+					return 0;
+				}
 
-                return price;
-            }
-        }
+				foreach (var item in OrderItems)
+				{
+					price += item.Amount * item.SalePrice.Price;
+				}
 
-        public Guid UserId { get; set; }
-        public List<OrderItem> OrderItems { get; set; }
-    }
+				return price;
+			}
+		}
+
+		public Guid UserId { get; set; }
+		public List<OrderItem> OrderItems { get; set; }
+	}
 }
